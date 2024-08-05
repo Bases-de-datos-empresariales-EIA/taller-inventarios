358a6bfcd4c45ab1cc160caedd5862dd98a9d465
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2024-08-05 06:43:23 -0500

    docs(ejercicios-sql.md): add new SQL exercise to find the supplier of the first purchase order in 2024 and the cost center where it was charged docs(ejercicios-sql.sql): implement SQL query to retrieve the supplier and cost center of the first purchase order in 2024

--- ejercicios-sql.md
+++ ejercicios-sql.md
@@ -2,6 +2,7 @@
 
 1. ¿Cuál es el primer proveedor con el que se negoció?
 2. ¿Cuál es la orden de compra más grande?
+3. ¿A qué proveedor se le compró la primera orden de compra del 2024? Adicionalmente, ¿en qué centro de costos se cargó esa OC?
 
 
 
--- ejercicios-sql.sql
+++ ejercicios-sql.sql
@@ -13,3 +13,16 @@ oc.codigo
 from inventarios."OrdenDeCompra" oc
 order by total desc
 limit 1
+
+-- ¿A qué proveedor se le compró la primera orden de compra del 2024? Adicionalmente, ¿en qué centro de costos se cargó esa OC?
+select
+t.nombre,
+cc.codigo
+from inventarios."OrdenDeCompra" oc 
+  join inventarios."Tercero" t 
+    on oc."terceroId" = t.id
+  join inventarios."CentroDeCostos" cc 
+    on oc."centroDeCostosId" = cc.id  
+where oc.fecha > '2024-01-01'
+order by oc.fecha asc
+limit 1;
