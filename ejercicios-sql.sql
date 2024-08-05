f3b89a618f81a3c6a7ed5fc6c40b7a73c73ca575
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2024-08-05 06:27:33 -0500

    docs(ejercicios-sql.sql): add SQL query to find the largest purchase order by total amount

--- ejercicios-sql.sql
+++ ejercicios-sql.sql
@@ -6,3 +6,10 @@ t.nombre
     on oc."terceroId" = t.id
 order by fecha asc
 limit 1;
+
+-- ¿Cuál es la orden de compra más grande?
+select 
+oc.codigo
+from inventarios."OrdenDeCompra" oc
+order by total desc
+limit 1
