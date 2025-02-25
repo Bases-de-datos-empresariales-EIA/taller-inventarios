d4b48e1ed99c72788b01c12634688efb5f0f9434
Daniel Saldarriaga López <dsaldarriaga@prevalentware.com>
2025-02-25 07:28:51 -0500

    Update ejercicios-sql.sql

--- ejercicios-sql.sql
+++ ejercicios-sql.sql
@@ -1,31 +1,76 @@
--- 1. ¿Cuál es el primer proveedor con el que se negoció?
+---------------- punto 1
+-- ¿Cuál es el primer proveedor con el que se negoció?
 select 
-t.nombre
- from inventarios."OrdenDeCompra" oc
-  join inventarios."Tercero" t
-    on oc."terceroId" = t.id
-order by fecha asc
-limit 1;
+t.nombre,
+oc.fecha
+from "Tercero" t
+	join "OrdenDeCompra" oc
+		on oc."terceroId"  = t.id
+order by oc.fecha asc
+limit 1
 
--- 2. ¿Cuál es la orden de compra más grande?
+---------------- punto 2
+-- ¿Cuál es la orden de compra más grande?
+select * from "OrdenDeCompra" oc
+order by oc.total desc
+limit 1
+
+
+---------------- punto 3
+-- ¿A qué proveedor se le compró la orden de compra más reciente? 
+-- Adicionalmente, ¿en qué centro de costos se cargó esa OC?
 select 
-oc.codigo
-from inventarios."OrdenDeCompra" oc
-order by total desc
-limit 1;
-
--- 3. ¿A qué proveedor se le compró la primera orden de compra del 2024? Adicionalmente, ¿en qué centro de costos se cargó esa OC?
-select
+oc.codigo,
+oc.fecha,
 t.nombre,
 cc.codigo
-from inventarios."OrdenDeCompra" oc 
-  join inventarios."Tercero" t 
-    on oc."terceroId" = t.id
-  join inventarios."CentroDeCostos" cc 
-    on oc."centroDeCostosId" = cc.id  
-where oc.fecha > '2024-01-01'
-order by oc.fecha asc
-limit 1;
+from "OrdenDeCompra" oc
+	join "Tercero" t
+		on oc."terceroId" = t.id
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+order by fecha desc
+limit 1
+
+
+-- ¿A qué proveedor se le compró la primera orden de compra del 2024? 
+-- Adicionalmente, ¿en qué centro de costos se cargó esa OC?
+
+-- filtro de fecha con between
+select * from "OrdenDeCompra" oc
+where oc.fecha between '2024-01-01' and '2024-12-31'
+order by fecha asc
+
+-- filtro de fecha con extract
+select 
+oc.codigo,
+oc.fecha,
+t.nombre,
+cc.codigo
+from "OrdenDeCompra" oc
+	join "Tercero" t
+		on oc."terceroId" = t.id
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where extract(year from oc.fecha) = 2024
+order by fecha asc
+limit 1
+
+-- obtener el proveedor y el centro de costos de la última compra del 2023
+select 
+oc.codigo,
+oc.fecha,
+t.nombre,
+cc.codigo
+from "OrdenDeCompra" oc
+	join "Tercero" t
+		on oc."terceroId" = t.id
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where extract(year from oc.fecha) = 2023
+order by fecha desc
+limit 1
+
 
 -- 4. ¿Cuál es el proveedor al que se le ha pagado la orden de compra más cara en el centro de costos CC-1001? ¿Cuál fue el valor de esa OC y en qué fecha se dio?
 select 
