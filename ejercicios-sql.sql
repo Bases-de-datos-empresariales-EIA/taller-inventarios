c414b0d559948d4357bf3bb537423e1f514baf6b
Daniel Saldarriaga López <dsaldarriaga@prevalentware.com>
2025-03-04 07:57:42 -0500

    Update ejercicios-sql.sql

--- ejercicios-sql.sql
+++ ejercicios-sql.sql
@@ -105,100 +105,170 @@ where cc.codigo = 'CC-1002'
 order by oc.total asc
 limit 1
 
--- 5. ¿Cuál es el total de compras realizadas en el centro de costos CC-1002?
-select 
-cc.codigo,
-sum(oc.total)
-from inventarios."CentroDeCostos" cc
-  join inventarios."OrdenDeCompra" oc
-    on oc."centroDeCostosId" = cc.id
-where cc.codigo = ' CC-1002'
-group by cc.codigo;
 
--- 6. Calcular el total de compras realizadas en cada centro de costos en el 2024.
+---------------- punto 5
+-- ¿Cuál es el total de compras realizadas en el centro de costos CC-1002?
+
+-- listar todas las compras del CC-1002
+select * from "OrdenDeCompra" oc
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where cc.codigo = 'CC-1002'
+
 select 
 cc.codigo,
 sum(oc.total)
-from inventarios."CentroDeCostos" cc
-  join inventarios."OrdenDeCompra" oc
-    on oc."centroDeCostosId" = cc.id
-where fecha >= '2024-01-01'
+from "OrdenDeCompra" oc
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where cc.codigo = 'CC-1002'
 group by cc.codigo
-order by sum(oc.total) desc;
 
--- 7. Calcular el total de compras realizadas por cada proveedor en el centro de costos CC-1003 en el año 2024.
-select 
+
+---------------- punto 6
+-- Calcular el total de compras realizadas en cada centro de costos en el 2024
+select
+cc.codigo,
+sum(oc.total)
+from "OrdenDeCompra" oc
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where extract(year from oc.fecha) = 2024
+group by cc.codigo
+
+-- cuántas compras se hicieron en cada centro de costos en 2021, 2022 y 2023?
+
+-- 2021
+select
+cc.codigo,
+count(oc.id)
+from "OrdenDeCompra" oc
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where extract(year from oc.fecha) = 2021
+group by cc.codigo
+
+-- 20222
+select
+cc.codigo,
+count(oc.id)
+from "OrdenDeCompra" oc
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where extract(year from oc.fecha) = 2022
+group by cc.codigo
+
+-- 2023
+select
+cc.codigo,
+count(oc.id)
+from "OrdenDeCompra" oc
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where extract(year from oc.fecha) = 2023
+group by cc.codigo
+
+
+---------------- punto 7
+-- Calcular el total de compras realizadas por cada proveedor en el centro de 
+-- costos CC-1003 en el año 2024.
+select
 t.nombre,
 sum(oc.total)
-from inventarios."OrdenDeCompra" oc
-  join inventarios."Tercero" t
-    on oc."terceroId" = t.id
-  join inventarios."CentroDeCostos" cc
-    on oc."centroDeCostosId" = cc.id
-where oc.fecha>'2024-01-01' and cc.codigo = ' CC-1003'
+from "OrdenDeCompra" oc
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+	join "Tercero" t
+		on oc."terceroId" = t.id
+where cc.codigo = 'CC-1003' and extract(year from oc.fecha) = 2024
 group by t.nombre
-order by sum(oc.total) desc;
 
--- 8. Calcular el total de compras realizadas en cada centro de costos para el proveedor Almacén RopaViva en el 2023.
+
+---------------- punto 8
+-- Calcular el total de compras realizadas por cada centro de costos para el proveedor Almacén RopaViva en el 2023.
 select 
 cc.codigo,
 sum(oc.total)
-from inventarios."OrdenDeCompra" oc
-  join inventarios."Tercero" t
-    on oc."terceroId" = t.id
-  join inventarios."CentroDeCostos" cc
-    on oc."centroDeCostosId" = cc.id
-where 
-t.nombre = ' Almacén RopaViva'
---and oc.fecha between '2023-01-01' and '2023-12-31'
-and extract(year from oc.fecha) = 2023
+from "Tercero" t
+	join "OrdenDeCompra" oc
+		on oc."terceroId" = t.id
+	join "CentroDeCostos" cc
+		on oc."centroDeCostosId" = cc.id
+where t.nombre = 'Almacén RopaViva' and extract(year from oc.fecha) = 2023
 group by cc.codigo
-order by sum(oc.total) desc;
 
--- 9. Calcular el mes del 2023 con más compras.
+
+---------------- punto 9
+-- Calcular el mes del 2023 con más compras.
 select 
 extract(month from oc.fecha) as "mes",
-sum(oc.total)
-from inventarios."OrdenDeCompra" oc
+sum(oc.total) as "total"
+from "OrdenDeCompra" oc
 where extract(year from oc.fecha) = 2023
 group by extract(month from oc.fecha)
 order by sum(oc.total) desc
-limit 1;
+limit 1
 
--- 10. Calcular el top 3 de proveedores a los que más se les ha comprado en el 2024.
+
+---------------- punto 10
+-- Calcular el top 3 de proveedores a los que más se les ha comprado en el 2024.
+-- seleccionar los terceros y join con las oc
+-- filtrar por 2024
+-- agrupar por nombre del tercero y calcular la suma
+-- ordenar por la suma descendente
+-- limit 3
 select 
 t.nombre,
 sum(oc.total)
-from inventarios."OrdenDeCompra" oc
-  join inventarios."Tercero" t
-    on oc."terceroId" = t.id
+from "Tercero" t
+	join "OrdenDeCompra" oc
+		on oc."terceroId" = t.id
 where extract(year from oc.fecha) = 2024
 group by t.nombre
 order by sum(oc.total) desc
-limit 3;
+limit 3
 
--- 11. Calcular el valor de la orden de compra más cara para cada proveedor.
-select 
-t.nombre,
-max(oc.total)
-from inventarios."OrdenDeCompra" oc
-  join inventarios."Tercero" t
-    on oc."terceroId" = t.id
-group by t.nombre;
 
--- 12. Calcular los 3 de centros de costos que menos se usaron durante el 2021.
+---------------- punto 11
+-- Calcular el valor de la orden de compra más cara para cada proveedor.
+-- seleccionar ordenes de compra y terceros
+-- agrupar por nombre del tercero
+-- en vez de la suma, calcular el max
+
+select
+t.nombre as proveedor,
+max(oc.total) as total
+from "OrdenDeCompra" oc
+join "Tercero" t
+	on oc."terceroId" = t.id
+group by t.nombre
+
+---------------- punto 12
+-- Calcular los 3 de centros de costos que menos se usaron durante el 2021.
+-- seleccionar ordenes de compra y centros de costos
+-- filtrar por ordenes de compra del 2021
+-- agrupar por codigo del centro de costos
+-- calular la suma
+-- ordernar por la suma
+-- limit 3
 select 
 cc.codigo,
 sum(oc.total)
-from inventarios."OrdenDeCompra" oc
-  join inventarios."CentroDeCostos" cc
-    on oc."centroDeCostosId" = cc.id
+from "CentroDeCostos" cc
+	join "OrdenDeCompra" oc
+		on oc."centroDeCostosId" = cc.id
 where extract(year from oc.fecha) = 2021
 group by cc.codigo
 order by sum(oc.total) asc
-limit 3;
+limit 3
 
 -- 13. ¿Cuáles son los 5 centros de costos con compras, en promedio, más altas?
+-- ¿Cuáles son los 5 centros de costos con compras, en promedio, más altas?
+-- seleccionar ordenes de compra y centros de costos
+-- agrupar por codigo de centro de costos
+-- calcular promedio
+-- ordenar por promedio de manera descendente
+-- limit 5
 select 
 cc.codigo,
 avg(oc.total)
@@ -210,6 +280,13 @@ order by avg(oc.total) desc
 limit 5;
 
 -- 14. ¿Cuáles son los 5 proveedores más baratos en promedio del 2023?
+-- ¿Cuáles son los 5 proveedores más baratos en promedio del 2023?
+-- seleccionar ordenes de compra y terceros
+-- filtrar por 2023
+-- agrupar por nombre del tercero
+-- calcular la suma
+-- ordenar por suma asc
+-- limit 5
 select 
 t.nombre,
 avg(oc.total)
@@ -222,6 +299,14 @@ order by avg(oc.total) asc
 limit 5;
 
 -- 15. ¿Cuáles son los 3 proveedores a los que más se les compró en enero del 2024?
+-- ¿Cuáles son los 3 proveedores a los que más se les compró en enero del 2024?
+-- seleccionar ordenes de compra y terceros
+-- filtar por 2024
+-- agrupar por nombre del tercero
+-- calcular la suma
+-- ordenar por suma desc
+-- limit 3
+
 select 
 t.nombre,
 sum(oc.total)
